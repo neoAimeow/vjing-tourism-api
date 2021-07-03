e984abfc9d02e4c50c812b28110a262fb9f15f81
wuzhaoyi02 <wuzhaoyi02@meituan.com>
2021-07-04 02:20:39 +0800

    edit seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,54 +4,33 @@ import * as dotenv from 'dotenv';
 const prisma = new PrismaClient();
 
 async function main() {
-  dotenv.config();
-  console.log('Seeding...');
+    dotenv.config();
+    console.log('Seeding...');
 
-  const user1 = await prisma.user.create({
-    data: {
-      email: 'lisa@simpson.com',
-      firstname: 'Lisa',
-      lastname: 'Simpson',
-      password: '$2b$10$EpRnTzVlqHNP0.fUbXUwSOyuiXe/QLSUG6xNekdHgTGmrpHEfIoxm', // secret42
-      role: 'USER',
-      posts: {
-        create: {
-          title: 'Join us for Prisma Day 2019 in Berlin',
-          content: 'https://www.prisma.io/day/',
-          published: true,
+    const user1 = await prisma.user.create({
+        data: {
+            email: 'lisa@simpson.com',
+            name: 'Simpson',
+            password:
+                '$2b$10$EpRnTzVlqHNP0.fUbXUwSOyuiXe/QLSUG6xNekdHgTGmrpHEfIoxm', // secret42
+            role: 'USER',
         },
-      },
-    },
-  });
-  const user2 = await prisma.user.create({
-    data: {
-      email: 'bart@simpson.com',
-      firstname: 'Bart',
-      lastname: 'Simpson',
-      role: 'ADMIN',
-      password: '$2b$10$EpRnTzVlqHNP0.fUbXUwSOyuiXe/QLSUG6xNekdHgTGmrpHEfIoxm', // secret42
-      posts: {
-        create: [
-          {
-            title: 'Subscribe to GraphQL Weekly for community news',
-            content: 'https://graphqlweekly.com/',
-            published: true,
-          },
-          {
-            title: 'Follow Prisma on Twitter',
-            content: 'https://twitter.com/prisma',
-            published: false,
-          },
-        ],
-      },
-    },
-  });
+    });
+    const user2 = await prisma.user.create({
+        data: {
+            email: 'bart@simpson.com',
+            name: 'Bart',
+            role: 'ADMIN',
+            password:
+                '$2b$10$EpRnTzVlqHNP0.fUbXUwSOyuiXe/QLSUG6xNekdHgTGmrpHEfIoxm', // secret42
+        },
+    });
 
-  console.log({ user1, user2 });
+    console.log({ user1, user2 });
 }
 
 main()
-  .catch((e) => console.error(e))
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+    .catch((e) => console.error(e))
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
